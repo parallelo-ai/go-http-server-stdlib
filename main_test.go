3cfc5f4701fbcc5906e9cf15f31cb4c4e32f7d9e
Daniel-M <danielmejia55@gmail.com>
2018-07-06 17:11:09 -0500

    Adding really simple testing

---
+++ main_test.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	//"io"
+	"io/ioutil"
+	//"net/http"
+	"net/http/httptest"
+	//"os"
+	"testing"
+)
+
+// TestGetMethod tests that the Server sends the correct response for the
+// GET query
+func TestGetMethod(t *testing.T) {
+	req := httptest.NewRequest("GET", "http://localhost:5555/", nil)
+	w := httptest.NewRecorder()
+
+	Home(w, req)
+
+	res := w.Result()
+	bodyByte, _ := ioutil.ReadAll(res.Body)
+
+	var bodyJSON []Response
+
+	err := json.Unmarshal(bodyByte, &bodyJSON)
+
+	if err != nil {
+		log.Fatal("Something happened parsing the response:", err)
+	}
+
+	if bodyJSON[0].Message != "Hello GET" {
+		t.Fatal("Home Handler sent the wrong response message", string(bodyByte))
+	}
+
+	if bodyJSON[0].Code != 200 {
+		t.Fatal("Home Handler sent the wrong response code", string(bodyByte))
+	}
+
+}
+
+func TestPostMethod(t *testing.T) {
+	req := httptest.NewRequest("POST", "http://localhost:5555/", nil)
+	w := httptest.NewRecorder()
+
+	Home(w, req)
+
+	res := w.Result()
+	bodyByte, _ := ioutil.ReadAll(res.Body)
+
+	var bodyJSON []Response
+
+	err := json.Unmarshal(bodyByte, &bodyJSON)
+
+	if err != nil {
+		log.Fatal("Something happened parsing the response:", err)
+	}
+
+	if bodyJSON[0].Message != "Hello GET" {
+		t.Fatal("Home Handler sent the wrong response message", string(bodyByte))
+	}
+
+	if bodyJSON[0].Code != 200 {
+		t.Fatal("Home Handler sent the wrong response code", string(bodyByte))
+	}
+
+}
